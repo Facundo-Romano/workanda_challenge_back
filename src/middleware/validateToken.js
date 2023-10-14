b0685e025696fa0d037a287556bef6a0de1f9024
Facundo-Romano <facuromano121299@gmail.com>
2023-10-14 17:43:51 -0300

    validateToken only uses authorization header

--- src/middleware/validateToken.js
+++ src/middleware/validateToken.js
@@ -4,7 +4,7 @@ import 'dotenv/config.js';
 const secretKey = process.env.SECRET_KEY;
 
 function validateToken(req, res, next) {
-  const token = req.headers.Authorization || req.cookies.jwt;
+  const token = req.headers.authorization;
 
   if (!token) {
     return res.status(401).json({ message: 'Unauthorized' });
